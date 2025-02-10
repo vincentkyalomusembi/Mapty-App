901248b2fcabb8a5faa922d7944638070ebce693
vincentkyalomusembi <vincentkyalo822@gmail.com>
2025-02-10 22:24:03 +0300

    updated script.js

--- script.js
+++ script.js
@@ -18,13 +18,15 @@ navigator.geolocation.getCurrentPosition(
         const { longitude } = position.coords;
         console.log(`https://www.google.com/maps/@${latitude},${longitude},`);
 
-        const map = L.map('map').setView([51.505, -0.09], 13);
+        const coords = [latitude, longitude];
+
+        const map = L.map('map').setView(coords, 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker(coords).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
 
